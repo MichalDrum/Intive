dc3c6bf36f90f8dce2caf401d890a2489c057cec
Michal <michaldrum@gmail.com>
2022-12-23 21:49:50 +0100

    Najnowsza wersja

---
+++ Registration/register.js
@@ -0,0 +1,31 @@
+function checkEmail() {
+	var email = document.getElementById("email").value;
+	var confirmEmail = document.getElementById("confirmEmail").value;
+	if (email != confirmEmail) {
+		alert("Pola E-mail nie są zgodne!");
+		return false;
+	}
+	return true;
+}
+
+function checkUsername() {
+	var userName = document.getElementById("login").value;
+	if (userName == "") {
+		alert("Nazwa użytkownika nie może być puste!");
+		return false;
+	}
+	return true;
+}
+
+function checkPassword() {
+	var password = document.getElementById("password").value;
+	if (password == "") {
+		alert("Pole hasło nie może być puste");
+		return false;
+	}
+	if (password.length < 6) {
+		alert("Hasło musi zawierać conajmniej 6 znaków!");
+		return false;
+	}
+	return true;
+}
