4740b28292c8825245d9de297225d7516a2ec528
Marcin P <podsiadmar@gmail.com>
2023-01-01 16:50:43 +0100

    localStorage, rejestracja

--- Registration/register.js
+++ Registration/register.js
@@ -1,24 +1,52 @@
-function checkEmail() {
+// function checkEmail() {
+// 	var email = document.getElementById("email").value;
+// 	var confirmEmail = document.getElementById("confirmEmail").value;
+// 	if (email != confirmEmail) {
+// 		alert("Pola E-mail nie są zgodne!");
+// 		return false;
+// 	}
+// 	return true;
+// }
+
+// function checkUsername() {
+// 	var userName = document.getElementById("login").value;
+// 	if (userName == "") {
+// 		alert("Nazwa użytkownika nie może być puste!");
+// 		return false;
+// 	}
+// 	return true;
+// }
+
+// function checkPassword() {
+// 	var password = document.getElementById("password").value;
+// 	if (password == "") {
+// 		alert("Pole hasło nie może być puste!");
+// 		return false;
+// 	}
+// 	if (password.length < 6) {
+// 		alert("Hasło musi zawierać conajmniej 6 znaków!");
+// 		return false;
+// 	}
+// 	return true;
+// }
+
+//Wrzucilem wszystko do jednej funkcji, zamiast wywoływać 3 funkcje, można po prostu jedną
+function validateRegisterForm(){
 	var email = document.getElementById("email").value;
 	var confirmEmail = document.getElementById("confirmEmail").value;
-	if (email != confirmEmail) {
-		alert("Pola E-mail nie są zgodne!");
-		return false;
-	}
-	return true;
-}
-
-function checkUsername() {
 	var userName = document.getElementById("login").value;
+	var password = document.getElementById("password").value;
+
 	if (userName == "") {
 		alert("Nazwa użytkownika nie może być puste!");
 		return false;
 	}
-	return true;
-}
 
-function checkPassword() {
-	var password = document.getElementById("password").value;
+	if (email != confirmEmail) {
+		alert("Pola E-mail nie są zgodne!");
+		return false;
+	}
+
 	if (password == "") {
 		alert("Pole hasło nie może być puste!");
 		return false;
@@ -28,4 +56,23 @@ function checkPassword() {
 		return false;
 	}
 	return true;
+}
+
+function setUserToLocalStorage(){
+	var email = document.getElementById("email").value;
+	var userName = document.getElementById("login").value;
+	var password = document.getElementById("password").value;
+
+	//check if username already exist
+	if (localStorage.getItem(userName) !== null) {
+		alert(`Nazwa użytwkonika już istnieje, proszę wybrać inną nazwę.`);
+		return false
+	}
+	if (localStorage.getItem(email) !== null) {
+		alert(`Adres e-mail jest już zajęty.`);
+		return false
+	}
+
+	window.localStorage.setItem(email, password);
+	window.localStorage.setItem(userName, password);
 }
