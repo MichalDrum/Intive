d89e449c61bf160fb0b5688037bd2c195e010497
Michal <michaldrum@gmail.com>
2023-01-02 20:15:21 +0100

    ZmianyWizualne

--- Registration/register.js
+++ Registration/register.js
@@ -1,36 +1,3 @@
-// function checkEmail() {
-// 	var email = document.getElementById("email").value;
-// 	var confirmEmail = document.getElementById("confirmEmail").value;
-// 	if (email != confirmEmail) {
-// 		alert("Pola E-mail nie są zgodne!");
-// 		return false;
-// 	}
-// 	return true;
-// }
-
-// function checkUsername() {
-// 	var userName = document.getElementById("login").value;
-// 	if (userName == "") {
-// 		alert("Nazwa użytkownika nie może być puste!");
-// 		return false;
-// 	}
-// 	return true;
-// }
-
-// function checkPassword() {
-// 	var password = document.getElementById("password").value;
-// 	if (password == "") {
-// 		alert("Pole hasło nie może być puste!");
-// 		return false;
-// 	}
-// 	if (password.length < 6) {
-// 		alert("Hasło musi zawierać conajmniej 6 znaków!");
-// 		return false;
-// 	}
-// 	return true;
-// }
-
-//Wrzucilem wszystko do jednej funkcji, zamiast wywoływać 3 funkcje, można po prostu jedną
 function validateRegisterForm(){
 	var email = document.getElementById("email").value;
 	var confirmEmail = document.getElementById("confirmEmail").value;
@@ -75,4 +42,5 @@ function setUserToLocalStorage(){
 
 	window.localStorage.setItem(email, password);
 	window.localStorage.setItem(userName, password);
+
 }
---
+++ signIn/login.js
@@ -0,0 +1,16 @@
+function signIn() {
+	var userName = document.getElementById("name").value;
+	var password = document.getElementById("login_password").value;
+	var localStoragePassword = window.localStorage.getItem(userName);
+
+	if (localStorage.getItem(userName) === null) {
+		alert(`Użytkownik nie istnieje!`);
+		return false;
+	}
+
+	if (password != localStoragePassword) {
+		alert("Podane hasło jest nieprawidłowe!");
+		return false;
+	}
+	return true;
+}
